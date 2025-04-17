6e8df99957f8ee32b04469bd7c31ada992579c43
Felix Barros <soyfelixbarros@gmail.com>
2025-04-17 13:01:00 -0300

    fix(responsive): agregar el meta viewport

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" type="image/x-icon">
   <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" type="image/x-icon">
   <?php wp_head(); ?>
