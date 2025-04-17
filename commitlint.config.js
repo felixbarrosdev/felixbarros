48054cadd873022ac4c0fc30ad7e05454df943b6
Felix Barros <soyfelixbarros@gmail.com>
2025-04-16 23:23:37 -0300

    fix: añade un ignores que filtre los commits de release

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,11 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-max-line-length': [2, 'always', 100]  // sigue validando normalmente
+    },
+    ignores: [
+        // omite la comprobación si el header empieza por "chore(release):"
+        (commit) => commit.startsWith('chore(release):')
+    ]
 };
   
