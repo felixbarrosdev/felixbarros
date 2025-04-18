52c8527bcabed643a93b232244d3b5dc5af7ebfd
Felix Barros <soyfelixbarros@gmail.com>
2025-04-18 20:26:56 -0300

    refactor: ajustar clases de contenedor en header y index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="flex-1 flex items-center justify-center px-4">
+<div class="flex-1 flex items-center justify-center p-4">
   <div class="max-w-3xl py-12 text-dark font-body">
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
